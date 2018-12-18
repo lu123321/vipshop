6f6d37069389ae4af958e7d89460a8e3975ba048
zuzong <562029730@qq.com>
2018-12-18 20:12:35 +0800

    Test and My

---
+++ wph_pay_consumer/src/main/java/com/example/wph_pay_consumer/cn/dao/Mydao.java
@@ -0,0 +1,6 @@
+package com.example.wph_pay_consumer.cn.dao;
+
+public interface Mydao {
+
+    String selAll(String name , String pwd);
+}
---
+++ wph_personal_consumer/src/main/java/com/example/wph_personal_consumer/cn/dao/TestDao.java
@@ -0,0 +1,5 @@
+package com.example.wph_personal_consumer.cn.dao;
+
+public interface TestDao {
+    String selAll(String name , String pwd);
+}
